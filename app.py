51dec7f393e83a41a266b3bbdff9661ab42d63f6
GEOVAN ARTABAN <juannunezvazquez2014@gmail.com>
2025-05-27 15:08:40 -0600

    Fix: removed __main__ block for deployment in Render

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-# app.py
 from flask import Flask, jsonify
 import requests
 import os
 
 app = Flask(__name__)
 
-NEWSAPI_KEY = "8be3c9b58cbc48b4bef066357d743946"
+NEWSAPI_KEY = os.getenv("NEWS_API_KEY")
 
 @app.route("/noticias")
 def obtener_noticias():
@@ -13,6 +12,3 @@ def obtener_noticias():
     response = requests.get(url)
     data = response.json()
     return jsonify(data)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
